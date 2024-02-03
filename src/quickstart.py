aee7f7c60649853b9426d1394f16514baf557ca9
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-02-03 10:55:07 +0200

    first commit

---
+++ src/quickstart.py
@@ -0,0 +1,38 @@
+import os
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.analytics.data_v1beta.types import (
+    DateRange,
+    Dimension,
+    Metric,
+    RunReportRequest,
+)
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'src/service_account.json'
+
+
+def sample_run_report(property_id="YOUR-GA4-PROPERTY-ID"):
+    """Runs a simple report on a Google Analytics 4 property."""
+    # TODO(developer): Uncomment this variable and replace with your
+    #  Google Analytics 4 property ID before running the sample.
+    property_id = property_id
+
+    # Using a default constructor instructs the client to use the credentials
+    # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    client = BetaAnalyticsDataClient()
+
+    request = RunReportRequest(
+        property=f"properties/{property_id}",
+        dimensions=[Dimension(name="city")],
+        metrics=[Metric(name="activeUsers")],
+        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+    )
+    response = client.run_report(request)
+
+    print("Report result:")
+    for row in response.rows:
+        print(row.dimension_values[0].value, row.metric_values[0].value)
+
+if __name__ == "__main__":
+    sample_run_report("426005599")
+
+
